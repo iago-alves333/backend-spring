5168115d79b472805692131307d3248ad2fa6746
iago <iago.alves@dcx.ufpb.br>
2026-07-16 17:34:34 -0300

    feat: adicao do ErrorResponseDTO

---
+++ src/main/java/br/ufpb/dcx/iago/lojadejogos/backend/dto/ErrorResponseDTO.java
@@ -0,0 +1,52 @@
+package br.ufpb.dcx.iago.lojadejogos.backend.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class ErrorResponseDTO {
+    private int status;
+    private String mensagem;
+    private LocalDateTime timestamp;
+    private List<String> erros;
+
+    public ErrorResponseDTO() {
+    }
+    public ErrorResponseDTO(int status, String mensagem, LocalDateTime timestamp, List<String> erros) {
+        this.status = status;
+        this.mensagem = mensagem;
+        this.timestamp = timestamp;
+        this.erros = erros;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public List<String> getErros() {
+        return erros;
+    }
+
+    public void setErros(List<String> erros) {
+        this.erros = erros;
+    }
+}
---
+++ src/main/java/br/ufpb/dcx/iago/lojadejogos/backend/exception/ResourceNotFoundException.java
@@ -0,0 +1,4 @@
+package br.ufpb.dcx.iago.lojadejogos.backend.exception;
+
+public class ResourceNotFoundException {
+}
